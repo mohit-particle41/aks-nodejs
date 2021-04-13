cd6d377d44fac8362acef9bb25f74a0eee3ad6f6
Mohit Choudhary <mohit@particle41.com>
2021-04-13 17:35:14 +0530

    Update files to test deployment workflow

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.get('/', (req, res) => res.send('NodeJS API AKS Example'+ Date()));
+app.get('/', (req, res) => res.send('NodeJS API AKS Example with Deployment center'+ Date()));
 app.get('/healthz', (req, res) => {
   const healthcheck = {
 		uptime: process.uptime(),
